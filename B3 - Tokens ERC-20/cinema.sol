635b1c0b3b5e8c664d529b6d74b407f8a92289d8
Joan Amengual <jamengual150899@gmail.com>
2022-01-17 15:06:21 +0100

    Update cinema.sol

--- B3 - Tokens ERC-20/cinema.sol
+++ B3 - Tokens ERC-20/cinema.sol
@@ -1,40 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts@4.4.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.4.2/access/Ownable.sol";
 
 contract Cinema is ERC20, Ownable {
     // ============================================
     // Initial Statements
     // ============================================
 
-    // Constructor
-    constructor() ERC20("Movies", "MC") {
+    // Constructor 
+    constructor () ERC20("Movies", "MC") {
         _mint(address(this), 1000);
     }
 
     // Data structure to store customer data
     struct customer {
         uint256 tokens_purchased;
-        string[] enjoyed_movies;
+        string [] enjoyed_movies;
     }
 
     // Mapping for customer registration
-    mapping(address => customer) public Customers;
+    mapping (address => customer) public Customers;
 
     // ============================================
-    // Token Management 
+    // Token Management
     // ============================================
 
     // Function to set the price of a Token
-    function priceTokens(uint256 _numTokens) internal pure returns (uint256) {
+    function priceTokens(uint256 _numTokens) internal pure returns (uint256){
         return _numTokens * (1 ether);
     }
 
-    // Function to view the account token balance
-    function balanceTokens(address _account) public view returns (uint256) {
-        return balanceOf(_account);
+    // Function to view the account token balance 
+    function balanceTokens(address _acount) public view returns (uint256){ 
+        return balanceOf(_acount);
     }
 
     // Function to mint more tokens
@@ -47,15 +47,16 @@ contract Cinema is ERC20, Ownable {
         // Set the price of the tokens
         uint256 cost = priceTokens(_numTokens);
         // The money that the customer pays for the tokens is evaluated
-        require(msg.value >= cost, "Buy less tokens or pay with more ethers.");
-        // Obtaining the number of tokens available from the Smart contract
+        require(msg.value >= cost, 
+                "Buy less tokens or pay with more ethers.");
+        // Obtaining the number of tokens available from the Smart Contract 
         uint256 balance = balanceTokens(address(this));
         require(_numTokens <= balance, "Buy a smaller number of tokens.");
-        // The returnValue is defined as what the customer pays minus what the product is worth.
+        // The returnValue is defined as what the customer pays minus what the product is worth
         uint256 returnValue = msg.value - cost;
-        // The company returns the amount of ethers to the customer
+        // The company send the amount of ethers to the customer 
         payable(msg.sender).transfer(returnValue);
-        // The number of tokens purchased is transferred to the customer
+        // The number of tokens purchased is transferred to the customer 
         _transfer(address(this), msg.sender, _numTokens);
         // Registration of purchased tokens
         Customers[msg.sender].tokens_purchased += _numTokens;
@@ -63,19 +64,14 @@ contract Cinema is ERC20, Ownable {
 
     // Function for a customer to exchange tokens for ethers
     function tokensEthers(uint256 _numTokens) public payable {
-        // Verify that the number of tokens to be returned is positive
-        require(
-            _numTokens > 0,
-            "You need to return a positive amount of tokens."
-        );
-        // The user must have the number of tokens to be returned
-        require(
-            _numTokens <= balanceTokens(msg.sender),
-            "You do not have the tokens you wish to return"
-        );
-        // Step-1: Sending tokens to the Smart contract
+        // Verify that the number of tokens is correct
+        require(_numTokens > 0, 
+                "You need to return a positive amount of tokens.");
+        require(_numTokens <= balanceTokens(msg.sender), 
+                "You do not have the tokens you wish to return.");
+        // Step-1: Sending tokens to the Smart Contract 
         _transfer(msg.sender, address(this), _numTokens);
-        // Step-2: Sending ethers to the customer
+        // Step-2: Sending ethers to the customer 
         payable(msg.sender).transfer(priceTokens(_numTokens));
     }
 
@@ -84,28 +80,25 @@ contract Cinema is ERC20, Ownable {
     // ============================================
 
     // Events
-    event enjoy_movie(string, uint256, address);
+    event enjoy_movie (string, uint256, address);
     event new_movie(string, uint256);
     event delete_movie(string);
 
-    // Data structure for movies
+    // Data structure for movies 
     struct movie {
         string movie_name;
         uint256 movie_price;
         bool movie_status;
     }
 
-    // Mapping to relate a movie name to a movie data structure
+    // Mapping to relate a movie name to a movie data structure 
     mapping(string => movie) public MappingMovies;
 
-    // Array for storing the name of the movies
-    string[] Movies;
+    // Array for storing the name of the movies 
+    string [] Movies;
 
-    // Incorporate new films into the cinema
-    function newMovie(string memory _movie_name, uint256 _movie_price)
-        public
-        onlyOwner
-    {
+    // Incorporate new movies into the Cinema
+    function newMovie (string memory _movie_name, uint256 _movie_price) public onlyOwner {
         // Creation of a new movie for the cinema
         MappingMovies[_movie_name] = movie(_movie_name, _movie_price, true);
         // Storing the movie name in an array
@@ -114,7 +107,7 @@ contract Cinema is ERC20, Ownable {
         emit new_movie(_movie_name, _movie_price);
     }
 
-    // Function to remove a movie from the cinema
+    // Function to remove a movie from the cinema 
     function deleteMovie(string memory _movie_name) public onlyOwner {
         // Movie status is set to FALSE => Not available in the cinema
         MappingMovies[_movie_name].movie_status = false;
@@ -122,20 +115,16 @@ contract Cinema is ERC20, Ownable {
         emit delete_movie(_movie_name);
     }
 
-    // Function to watch a movie and pay tokens for it
+    // Function to watch a movie and pay tokens for it 
     function watchMovie(string memory _movie_name) public {
+        // Verify the status of the movie (if it is available for watching)
+        require(MappingMovies[_movie_name].movie_status == true, 
+                "This is not available in this cinema.");
         // Price of the movie (in tokens)
         uint256 movie_tokens = MappingMovies[_movie_name].movie_price;
-        // Verify the status of the movie (if it is available for watching)
-        require(
-            MappingMovies[_movie_name].movie_status == true,
-            "This movie is not available in this cinema."
-        );
-        // Verify the number of tokens the client has to watch the movie
-        require(
-            movie_tokens <= balanceTokens(address(this)),
-            "You need more tokens to watch this movie."
-        );
+         // Verify the number of tokens the client has to watch the movie
+        require(movie_tokens <= balanceTokens(msg.sender), 
+                "You need more tokens to watch this movie.");
         _transfer(msg.sender, address(this), movie_tokens);
         // Storage in the client's history of watched movies
         Customers[msg.sender].enjoyed_movies.push(_movie_name);
@@ -148,12 +137,19 @@ contract Cinema is ERC20, Ownable {
     // ============================================
 
     // Function to visualize the movie history
-    function movieHistory() public view returns (string[] memory) {
-        return Customers[msg.sender].enjoyed_movies;
+    function movieHistory(address _account) public view returns (string [] memory) {
+        return Customers[_account].enjoyed_movies;
     }
 
-    // Function for displaying movies available in the cinema
-    function cinema_schedule() public view returns (string[] memory) {
+    // Function to visualize movies available in the cinema
+    function cinema_schedule() public view returns (string [] memory){
         return Movies;
     }
+
+    // Extraction of ethers from the Smart Contract to the Owner
+    function withdraw() external payable onlyOwner{
+        address payable _owner = payable(owner());
+        _owner.transfer(address(this).balance);
+    }
+
 }
